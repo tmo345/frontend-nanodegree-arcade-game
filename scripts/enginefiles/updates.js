77d696ff5a6fb67c0ba08986d746591d9c18d4bd
timo31415 <moore.tim@outlook.com>
2016-01-30 15:54:17 +0000

    Moved score updating to score.js and changed to storing coordinates in properties of score object as in endscreen and startscreen

--- scripts/enginefiles/updates.js
+++ scripts/enginefiles/updates.js
@@ -1,32 +1,16 @@
-var app = require('../app.js'),
-    canvas = require('../gamedata/canvas.js');
+var app = require('../app.js');
 
-    // Class instances
-var player = app.player,
-    allEnemies = app.allEnemies,
-    score = app.score;
 
 function updateEntities(dt) {
-    allEnemies.forEach(function(enemy) {
+    app.allEnemies.forEach(function(enemy) {
         enemy.update(dt);
     });
-    player.update();
+    app.player.update();
 }
 
 function updateGameInformation() {
     app.gameTimer.update();
-    updateScore();
-}
-
-function updateScore() {
-    var playerInTheWater = player.getInWaterStatus(),
-        playerCollidedWithEnemy = player.getCollisionStatus();
-
-    if (playerInTheWater) {
-        score.update('up', 100);
-    } else if (playerCollidedWithEnemy) {
-        score.update('down', 50);
-    }
+    app.score.update(app.player);
 }
 
 module.exports = {
--- scripts/gamedata/score.js
+++ scripts/gamedata/score.js
@@ -5,12 +5,20 @@ var renderHelper = require('../utilities/renderhelper.js'),
 var gameScore = {
 
     currentScore: 0,
+    scoreUpAmount: 100,
+    scoreDownAmount: 50,
 
-    headingX: landmarks.xLeftSideOf.tile1 + 10,
-    headingY: landmarks.yTopOf.water + 50,
+    heading: {
+        text: 'Score: ',
+        x: landmarks.xLeftSideOf.tile1 + 10,
+        y: landmarks.yTopOf.water + 50
+    },
 
-    scoreX: landmarks.xLeftSideOf.tile2 + 10,
-    scoreY: landmarks.yTopOf.water + 50,
+    scoreText: {
+        fillerSpace: ' ',
+        x: landmarks.xLeftSideOf.tile2 + 10,
+        y: landmarks.yTopOf.water + 50
+    },
 
     highlightedGreen: false,
     highlightedRed: false,
@@ -29,11 +37,42 @@ var gameScore = {
         this.currentScore = 0;
     },
 
-    render: function(ctx) {
-        renderHelper.setDefaultStyles(ctx);
+    changeScore: function(directionOfChange) {
+        var that = this;
 
-        ctx.strokeText('Score :', this.headingX, this.headingY);
-        ctx.fillText('Score :', this.headingX, this.headingY);
+        if (directionOfChange === 'up') {
+            this.currentScore += this.scoreUpAmount;
+            this.highlightedGreen = true;
+            // Green score flash and larger font for 350ms
+            window.setTimeout(function() {
+                that.highlightedGreen = false;
+            }, 350);
+        } else if (directionOfChange === 'down') {
+            this.currentScore -= this.scoreDownAmount;
+            this.highlightedRed = true;
+            // Red score flash and larger font for 350ms
+            window.setTimeout(function() {
+                that.highlightedRed = false;
+            }, 350);
+        }
+    },
+
+    // Expect an instance of player with getInWaterStatus and getCollisionStatus methods
+    update: function(player) {
+        var playerInTheWater = player.getInWaterStatus(),
+        playerCollidedWithEnemy = player.getCollisionStatus();
+
+        if (playerInTheWater) {
+            this.changeScore('up');
+        } else if (playerCollidedWithEnemy) {
+            this.changeScore('down');
+        }
+    },
+
+    render: function(ctx) {
+
+        ctx.strokeText(this.heading.text, this.heading.x, this.heading.y);
+        ctx.fillText(this.heading.text, this.heading.x, this.heading.y);
 
         // Highlighted green and red properties toggled in update method
         // when score changes
@@ -46,31 +85,11 @@ var gameScore = {
         // Add extra space for positive so that negatives don't seem to shift
         // over to the right with negative sign
         if (this.score >= 0) {
-            ctx.strokeText(' ' + this.currentScore, this.scoreX, this.scoreY);
-            ctx.fillText(' ' + this.currentScore, this.scoreX, this.scoreY);
+            ctx.strokeText(this.scoreText.fillerSpace + this.currentScore, this.scoreText.x, this.scoreText.y);
+            ctx.fillText(this.scoreText.fillerSpace + this.currentScore, this.scoreText.x, this.scoreText.y);
         } else {
-            ctx.strokeText(this.currentScore, this.scoreX, this.scoreY);
-            ctx.fillText(this.currentScore, this.scoreX, this.scoreY);
-        }
-    },
-
-    update: function(directionOfChange, amount) {
-        var that = this;
-
-        if (directionOfChange === 'up') {
-            this.currentScore += amount;
-            this.highlightedGreen = true;
-            // Green score flash and larger font for 350ms
-            window.setTimeout(function() {
-                that.highlightedGreen = false;
-            }, 350);
-        } else if (directionOfChange === 'down') {
-            this.currentScore -= amount;
-            this.highlightedRed = true;
-            // Red score flash and larger font for 350ms
-            window.setTimeout(function() {
-                that.highlightedRed = false;
-            }, 350);
+            ctx.strokeText(this.currentScore, this.scoreText.x, this.scoreText.y);
+            ctx.fillText(this.currentScore, this.scoreText.x, this.scoreText.y);
         }
     }
 };
