6ac23aca7a145214938232997d3657633164eac5
timo31415 <moore.tim@outlook.com>
2016-02-09 22:42:30 +0000

    Player now subscribes to collisionOccured and playerReached water to monitor for these state changes.

--- scripts/gameelements/player.js
+++ scripts/gameelements/player.js
@@ -5,39 +5,50 @@ var landmarks = require('../utilities/landmarks.js'),
 var Player = function() {
     this.sprite = 'images/char-boy.png';
 
-    // this.collidedWithEnemy = false;
-    // this.inTheWater = false;
+    this.collidedWithEnemy = false;
+    this.inTheWater = false;
 
     this.resetSprite();
 };
 
+Player.prototype.setSubscriptions = function(stateTracker) {
+    var toggleBound = this.toggleCollisionStatus.bind(this);
+    try {
+        stateTracker.subscribe('collisionOccured', toggleBound);
+        stateTracker.subscribe('playerReachedWater', toggleBound);
+    } catch (e) {
+        console.error(e.message);
+    }
+
+};
 
 
-// Player.prototype.toggleCollisionStatus = function() {
-//     if (! this.collidedWithEnemy) {
+Player.prototype.toggleCollisionStatus = function() {
 
-//         this.collidedWithEnemy = true;
-//     } else {
-//         this.collidedWithEnemy = false;
-//     }
-// };
+    if (! this.collidedWithEnemy) {
 
-// Player.prototype.togglePlayerInWaterStatus = function() {
-//     if (! this.inTheWater) {
-//         this.inTheWater = true;
-//     } else {
-//         this.inTheWater = false;
-//     }
-// };
+        this.collidedWithEnemy = true;
+    } else {
+        this.collidedWithEnemy = false;
+    }
+};
+
+Player.prototype.togglePlayerInWaterStatus = function() {
+    if (! this.inTheWater) {
+        this.inTheWater = true;
+    } else {
+        this.inTheWater = false;
+    }
+};
 
 
-// Player.prototype.getInWaterStatus = function() {
-//     return this.inTheWater;
-// };
+Player.prototype.getInWaterStatus = function() {
+    return this.inTheWater;
+};
 
-// Player.prototype.getCollisionStatus = function() {
-//     return this.collidedWithEnemy;
-// };
+Player.prototype.getCollisionStatus = function() {
+    return this.collidedWithEnemy;
+};
 
 
 Player.prototype.resetSprite = function(){
@@ -52,7 +63,8 @@ Player.prototype.update = function(collisions) {
     var oneTileX = 101;
     var oneTileY = 83;
 
-    if (collisions.collisionOccured() || collisions.playerReachedWater()) {
+    if (this.collidedWithEnemy) {
+        console.log(this.collidedWithEnemy);
         this.resetSprite();
     }
 
