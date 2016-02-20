e4a43fac450585552ff605ea2a8f8817f293de05
timo31415 <moore.tim@outlook.com>
2016-02-20 15:08:42 +0000

    Move update functions from handlers.js to updates.js

--- scripts/engine_logic/handlers.js
+++ scripts/engine_logic/handlers.js
@@ -1,27 +1,10 @@
-var entities = require('../game_objects/instantiate_entities');
 var score = require('../game_objects/score');
 var timer = require('../game_objects/timer');
 
 
 
 
-var player = entities.player,
-    allEnemies = entities.allEnemies;
 
-
-
-// Perform updates on game element information based on changes since last frame
-function updateEntities(dt, statePubSub) {
-    allEnemies.forEach(function(enemy) {
-        enemy.update(dt);
-    });
-    player.update(statePubSub);
-}
-
-function updateGameInformation(statePubSub) {
-    timer.update();
-    // score.update(statePubSub);
-}
 
 
 
@@ -39,8 +22,7 @@ function resetGameInformation() {
 
 
 module.exports = {
-    updateEntities: updateEntities,
-    updateGameInformation: updateGameInformation,
+
     resetGameInformation: resetGameInformation,
 
 };
--- scripts/engine_logic/updates.js
+++ scripts/engine_logic/updates.js
@@ -1,2 +1,26 @@
 'use strict';
 
+var entities = require('../game_objects/instantiate_entities');
+var timer = require('../game_objects/timer');
+
+var player = entities.player,
+    allEnemies = entities.allEnemies;
+
+
+
+// Perform updates on game element information based on changes since last frame
+function updateEntities(dt, statePubSub) {
+    allEnemies.forEach(function(enemy) {
+        enemy.update(dt);
+    });
+    player.update(statePubSub);
+}
+
+function updateGameInformation() {
+    timer.update();
+}
+
+module.exports = {
+    updateEntities: updateEntities,
+    updateGameInformation: updateGameInformation
+};
